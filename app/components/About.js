11bea298c6c392d3c33447a6cfc8d162fc5f84e9
nerokome <nerooghenekome@gmail.com>
2025-05-02 15:15:25 +0100

    bruh

--- app/components/About.js
+++ app/components/About.js
@@ -4,16 +4,21 @@ import Heading from './Heading'
 const About = () => {
   return (
     <div id='about' className='mb-12'>
-      <section className='flex items-center justify-center'>
+      <section className='flex flex-col items-center justify-center sm:flex-row'>
         <Heading title="About Me" />
         <div className=''></div>
       </section>
-      <div className='px-10 mb-50'>
-        <h4 className='text-sm text-white sm:text-base'>
-          I'm Nero Oghenekome, I am a dedicated software developer with years of experience in designing and building innovative web and mobile applications. Specializing in web and mobile app development, 
-          I am proficient in a wide range of technologies, including JavaScript, React, and Flutter, enabling the creation of responsive and scalable solutions tailored to client needs.
-          Having worked on projects ranging from e-commerce platforms to productivity apps, a strong advocate for clean code and agile methodologies, I thrive in collaborative environments.
-          Beyond coding, I am passionate about staying at the forefront of tech trends, frequently exploring advancements in artificial intelligence and cloud computing. In their free time, I enjoy contributing to open-source projects, mentoring aspiring developers, and sharing insights on tech blogs. 
+      <div className='px-4 sm:px-10 mb-50'>
+        <h4 className='text-sm text-white sm:text-base leading-relaxed'>
+          I'm Nero Oghenekome, a dedicated software developer with years of experience in designing and building innovative web and mobile applications. 
+          Specializing in web and mobile app development, I am proficient in a wide range of technologies, including JavaScript, React, and Flutter, enabling the creation of responsive and scalable solutions tailored to client needs.
+        </h4>
+        <h4 className='text-sm text-white sm:text-base leading-relaxed mt-4'>
+          Having worked on projects ranging from e-commerce platforms to productivity apps, I am a strong advocate for clean code and agile methodologies, thriving in collaborative environments. 
+          Beyond coding, I am passionate about staying at the forefront of tech trends, frequently exploring advancements in artificial intelligence and cloud computing.
+        </h4>
+        <h4 className='text-sm text-white sm:text-base leading-relaxed mt-4'>
+          In my free time, I enjoy contributing to open-source projects, mentoring aspiring developers, and sharing insights on tech blogs. 
           With a commitment to continuous learning and innovation, I am always ready to tackle new challenges in the ever-evolving world of software development.
         </h4>
       </div>
--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -11,77 +11,77 @@ const Navbar = () => {
    setNavOpen(!navOpen);
   };
 return (
-   <div className={`left-0 top-0 w-full z-10 ease-in duration-300 ${navOpen ? 'h-screen overflow-hidden' : ''}`}>
-      <div className='max-w-[1240px] m-auto flex justify-between items-center p-4'>
+   <div className={`left-0 top-0 w-full z-10 ease-in-out duration-300 ${navOpen ? 'h-screen overflow-hidden' : ''}`}>
+      <div className='max-w-[1240px] m-auto flex justify-between items-center p-4 ease-in-out duration-300'>
           <Link href= '/'>
-          <h1 className='text-4xl text-gray-400'>Nero Oghenekome</h1>
+          <h1 className='text-4xl text-gray-400 ease-in-out duration-300'>Nero Oghenekome</h1>
           </Link>
-          <ul className='hidden sm:flex'>
+          <ul className='hidden sm:flex ease-in-out duration-300'>
                <li>
                    <Link href='/'>
-                   <h1 className='font-mono text-amber-50 text-2xl p-4 hover:text-purple-400 cursor-grab'>Home</h1>
+                   <h1 className='font-mono text-amber-50 text-2xl p-4 hover:text-purple-400 cursor-grab ease-in-out duration-300'>Home</h1>
                    </Link>
                </li>
                <li>
                    <Link href='/#about'>
-                   <h1 className=' font-mono text-amber-50 text-2xl p-4  hover:text-purple-400 cursor-grab'>About</h1>
+                   <h1 className=' font-mono text-amber-50 text-2xl p-4  hover:text-purple-400 cursor-grab ease-in-out duration-300'>About</h1>
                    </Link>
                </li>
                <li>
                    <Link href='/#skills'>
-                   <h1 className=' font-mono text-amber-50 text-2xl p-4  hover:text-purple-400 cursor-grab'>Skills</h1>
+                   <h1 className=' font-mono text-amber-50 text-2xl p-4  hover:text-purple-400 cursor-grab ease-in-out duration-300'>Skills</h1>
                    </Link>
                </li>
                <li>
                    <Link href='/#projects'>
-                   <h1 className= ' font-mono text-amber-50 text-2xl p-4  hover:text-purple-400 cursor-grab'>Projects</h1>
+                   <h1 className= ' font-mono text-amber-50 text-2xl p-4  hover:text-purple-400 cursor-grab ease-in-out duration-300'>Projects</h1>
                    </Link>
                </li>
           <li>
                <Link href='/#contact'>
-               <h1 className=' font-mono text-amber-50 text-2xl p-4  hover:text-purple-400 cursor-grab'>Contact</h1>
+               <h1 className=' font-mono text-amber-50 text-2xl p-4  hover:text-purple-400 cursor-grab ease-in-out duration-300'>Contact</h1>
                </Link>
           </li>
       </ul>
-      <div className='block sm:hidden z-10' onClick={toggleNav}>
+      <div className='block sm:hidden z-10 ease-in-out duration-300' onClick={toggleNav}>
       <CiMenuBurger size={20} color='white' />
       </div>
       {navOpen && (
          <div
             className='absolute top-0 left-0 right-0 bottom-0 flex justify-center
-            items-center w-full h-screen bg-black text-center ease-in duration-300 sm:hidden'
+            items-center w-full h-screen bg-black text-center ease-in-out duration-300 sm:hidden'
          >
             <ul>
                <li>
                   <Link href='/'>
-                  <h1 className='font-mono text-amber-50 p-4 text-4xl hover:text-gray-500'>Home</h1>
+                  <h1 className='font-mono text-amber-50 p-4 text-4xl hover:text-gray-500 ease-in-out duration-300'>Home</h1>
                   </Link>
                </li>
                <li>
                   <Link href='/#about'>
                   <h1 className=' font-mono text-amber-50 p-4 text-4xl
-                   hover:text-gray-500 '>About</h1>
+                   hover:text-gray-500 ease-in-out duration-300'>About</h1>
                   </Link>
                </li>
                <li>
-                  <Link href='/#contact'>
-                  <h1 className=' font-mono text-amber-50 p-4 text-4xl hover:text-gray-500'>Skills</h1>
+                  <Link href='/#skills'>
+                  <h1 className=' font-mono text-amber-50 p-4 text-4xl hover:text-gray-500 ease-in-out duration-300'>Skills</h1>
                   </Link>
                </li>
                <li>
                   <Link href='/#projects'>
-                  <h1 className= ' font-mono text-amber-50 p-4 text-4xl hover:text-gray-500'>Projects</h1>
+                  <h1 className= ' font-mono text-amber-50 p-4 text-4xl hover:text-gray-500 ease-in-out duration-300'>Projects</h1>
                   </Link>
                </li>
                <li>
                   <Link href='/#contact'>
-                  <h1 className= ' font-mono text-amber-50 p-4 text-4xl hover:text-gray-500'>Contact</h1>
+                  <h1 className= ' font-mono text-amber-50 p-4 text-4xl hover:text-gray-500 ease-in-out duration-300'>Contact</h1>
                   </Link>
                   
                </li>
                <li>
                   <Link href='https://drive.google.com/file/d/1ou7ISlZG4gFGjjEOVpTr7V5KQkCoFM7x/view?usp=sharing'>
-                  <h1 className= ' font-mono text-amber-50 p-4 text-4xl hover:text-gray-500'>CV</h1>
+                  <h1 className= ' font-mono text-amber-50 p-4 text-4xl hover:text-gray-500 ease-in-out duration-300'>CV</h1>
                   </Link>
                   
                </li>
@@ -92,7 +92,7 @@ return (
                 href="https://drive.google.com/file/d/1ou7ISlZG4gFGjjEOVpTr7V5KQkCoFM7x/view?usp=sharing"
                 className="hidden sm:flex items-center border bg-green-300 text-white
                   px-4 py-2 mr-2.5 rounded-lg hover:bg-blue-300 
-                  transition-colors duration-200 font-medium text-center "
+                  transition-colors duration-200 font-medium text-center ease-in-out"
                   
               >
                <h1 className='text-center'>Download CV </h1>
