40337013642f0b727c1f66fa701e8cf5d49716cd
nerokome <nerooghenekome@gmail.com>
2025-05-02 21:53:34 +0100

    pan

--- app/components/Skills.js
+++ app/components/Skills.js
@@ -7,58 +7,58 @@ return (
         <section className='flex items-center justify-center'>
             <Heading title="Technical Skills" />
         </section>
-        <div className='grid grid-cols-2 lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-2 px-10 gap-6 animate-pulse justify-center'>
+        <div className='grid grid-cols-2 lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-2 px-10 gap-6  justify-center'>
         <div className='flex items-center space-x-4 rounded-lg  hover:bg-gray-900 transition-colors duration-300'>
                                 
-                                        <img className='w-[50px] h-[50px] rounded-full' src="/HTML.png" alt="Download" />
+                                        <img className='w-[40px] h-[40px] rounded-full' src="/HTML.png" alt="Download" />
                                 
                                 <h2 className='text-xl text-amber-50'>HTML</h2>
                         </div>
                         <div className='flex items-center space-x-4 rounded-lg  hover:bg-gray-900 transition-colors duration-300'>
                                 
-                                        <img className='w-[50px] h-[50px] rounded-full' src="/CSS.png" alt="Download" />
+                                        <img className='w-[40px] h-[40px] rounded-full' src="/CSS.png" alt="Download" />
                                 
                                 <h2 className='text-xl text-amber-50'>CSS</h2>
                         </div>
                         <div className='flex items-center space-x-4 rounded-lg  hover:bg-gray-900 transition-colors duration-300'>
                                 
-                                        <img className='w-[50px] h-[50px] rounded-full' src="/react.png" alt="Download" />
+                                        <img className='w-[40px] h-[40px] rounded-full animate-spin' src="/react.png" alt="Download" />
                                 
                                 <h2 className='text-xl text-amber-50'>React</h2>
                         </div>
                         <div className='flex items-center space-x-4 rounded-lg  hover:bg-gray-900 transition-colors duration-300'>
                                 
-                                        <img className='w-[50px] h-[50px] rounded-full' src="/Nextjs.png" alt="Download" />
+                                        <img className='w-[40px] h-[40px] rounded-full' src="/Nextjs.png" alt="Download" />
                                 
                                 <h2 className='text-xl text-amber-50'>Next JS</h2>
                         </div>
                         <div className='flex items-center space-x-4 rounded-lg  hover:bg-gray-900 transition-colors duration-300'>
                                 
-                                        <img className='w-[50px] h-[50px] rounded-full' src="/Firebase.png" alt="Download" />
+                                        <img className='w-[40px] h-[40px] rounded-full' src="/Firebase.png" alt="Download" />
                                 
                                 <h2 className='text-xl text-amber-50'>Firebase</h2>
                         </div>
                         <div className='flex items-center space-x-4 rounded-lg  hover:bg-gray-900 transition-colors duration-300'>
                                 
-                                        <img className='w-[50px] h-[50px] rounded-full' src="/JS.png" alt="Download" />
+                                        <img className='w-[40px] h-[40px] rounded-full' src="/JS.png" alt="Download" />
                                 
                                 <h2 className='text-xl text-amber-50'>Javascript</h2>
                         </div>
                         <div className='flex items-center space-x-4 rounded-lg  hover:bg-gray-900 transition-colors duration-300'>
                                 
-                                        <img className='w-[50px] h-[50px] rounded-full' src="/Tail.png" alt="Download" />
+                                        <img className='w-[40px] h-[40px] rounded-full' src="/Tail.png" alt="Download" />
                                 
                                 <h2 className='text-xl text-amber-50'>Tailwind CSS</h2>
                         </div>
                         <div className='flex items-center space-x-4 rounded-lg  hover:bg-gray-900 transition-colors duration-300'>
                                 
-                                        <img className='w-[50px] h-[50px] rounded-full' src="/flutter.png" alt="Download" />
+                                        <img className='w-[40px] h-[40px] rounded-full' src="/flutter.png" alt="Download" />
                                 
                                 <h2 className='text-xl text-amber-50'>Dart</h2>
                         </div>
                         <div className='flex items-center space-x-4 rounded-lg  hover:bg-gray-900 transition-colors duration-300'>
                                 
-                                        <img className='w-[50px] h-[50px] rounded-full' src="/download.png" alt="Download" />
+                                        <img className='w-[40px] h-[40px] rounded-full' src="/download.png" alt="Download" />
                                 
                                 <h2 className='text-xl text-amber-50'>Github</h2>
                         </div>
