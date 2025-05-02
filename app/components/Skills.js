b8bfe1151f1d196bf2fa0ec56c70ee4c6929bc3c
nerokome <nerooghenekome@gmail.com>
2025-05-02 20:45:22 +0100

    change

--- app/components/Skills.js
+++ app/components/Skills.js
@@ -1,49 +1,67 @@
 import React from 'react'
 import Heading from './Heading'
-import { FaFlutter } from "react-icons/fa6";
-import { FaHtml5 } from "react-icons/fa";
-import { FaCss3Alt } from "react-icons/fa";
-import { RiNextjsFill } from "react-icons/ri";
-import { FaReact } from "react-icons/fa";
-import { IoLogoJavascript } from "react-icons/io";
-import { RiTailwindCssFill } from "react-icons/ri";
-import { RiFirebaseLine } from "react-icons/ri";
-import { FaGithub } from "react-icons/fa";
 
 const Skills = () => {
 return (
     <div id='skills' className='mb-50 sm:mb-0'>
         <section className='flex items-center justify-center'>
-            <Heading title="Skills" />
+            <Heading title="Technical Skills" />
         </section>
-        <div className='grid grid-cols-3 lg:grid-cols-3 md:grid-cols-3 sm:grid-cols-2 px-10 gap-6 animate-pulse justify-center'>
-            <div className='shadow-lg flex flex-col gap-4 px-10 py-8 animate-pulse'>
-                <FaFlutter className='text-blue-500' style={{ fontSize: '3rem' }} />
-            </div>
-            <div className='shadow-lg flex flex-col gap-4 px-10 py-8'>
-                <FaHtml5 className='text-red-500' style={{ fontSize: '3rem' }} />
-            </div>
-            <div className='shadow-lg flex flex-col gap-4 px-10 py-8 animate-pulse'>
-                <FaCss3Alt className='text-blue-500' style={{ fontSize: '3rem' }} />
-            </div>
-            <div className='shadow-lg flex flex-col gap-4 px-10 py-8'>
-                <RiNextjsFill className='text-gray-500' style={{ fontSize: '3rem' }} />
-            </div>
-            <div className='shadow-lg flex flex-row gap-4 px-10 py-8 animate-pulse'>
-                <FaReact className='text-blue-500' style={{ fontSize: '3rem' }} />
-            </div>
-            <div className='shadow-lg flex flex-row gap-4 px-10 py-8'>
-                <IoLogoJavascript className='text-yellow-500' style={{ fontSize: '3rem' }} />
-            </div>
-            <div className='shadow-lg flex flex-col gap-4 px-10 py-8 animate-pulse'>
-                <RiTailwindCssFill className='text-blue-500' style={{ fontSize: '3rem' }} />
-            </div>
-            <div className='shadow-lg flex flex-col gap-4 px-10 py-8'>
-                <RiFirebaseLine className='text-red-500' style={{ fontSize: '3rem' }} />
-            </div>
-            <div className='shadow-lg flex flex-col gap-4 px-10 py-8 animate-pulse'>
-                <FaGithub className='text-white' style={{ fontSize: '3rem' }} />
-            </div>
+        <div className='grid grid-cols-2 lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-2 px-10 gap-6 animate-pulse justify-center'>
+        <div className='flex items-center space-x-4 rounded-lg  hover:bg-gray-900 transition-colors duration-300'>
+                                
+                                        <img className='w-[50px] h-[50px] rounded-full' src="/HTML.png" alt="Download" />
+                                
+                                <h2 className='text-xl text-amber-50'>HTML</h2>
+                        </div>
+                        <div className='flex items-center space-x-4 rounded-lg  hover:bg-gray-900 transition-colors duration-300'>
+                                
+                                        <img className='w-[50px] h-[50px] rounded-full' src="/CSS.png" alt="Download" />
+                                
+                                <h2 className='text-xl text-amber-50'>CSS</h2>
+                        </div>
+                        <div className='flex items-center space-x-4 rounded-lg  hover:bg-gray-900 transition-colors duration-300'>
+                                
+                                        <img className='w-[50px] h-[50px] rounded-full' src="/react.png" alt="Download" />
+                                
+                                <h2 className='text-xl text-amber-50'>React</h2>
+                        </div>
+                        <div className='flex items-center space-x-4 rounded-lg  hover:bg-gray-900 transition-colors duration-300'>
+                                
+                                        <img className='w-[50px] h-[50px] rounded-full' src="/Nextjs.png" alt="Download" />
+                                
+                                <h2 className='text-xl text-amber-50'>Next JS</h2>
+                        </div>
+                        <div className='flex items-center space-x-4 rounded-lg  hover:bg-gray-900 transition-colors duration-300'>
+                                
+                                        <img className='w-[50px] h-[50px] rounded-full' src="/Firebase.png" alt="Download" />
+                                
+                                <h2 className='text-xl text-amber-50'>Firebase</h2>
+                        </div>
+                        <div className='flex items-center space-x-4 rounded-lg  hover:bg-gray-900 transition-colors duration-300'>
+                                
+                                        <img className='w-[50px] h-[50px] rounded-full' src="/JS.png" alt="Download" />
+                                
+                                <h2 className='text-xl text-amber-50'>Javascript</h2>
+                        </div>
+                        <div className='flex items-center space-x-4 rounded-lg  hover:bg-gray-900 transition-colors duration-300'>
+                                
+                                        <img className='w-[50px] h-[50px] rounded-full' src="/Tail.png" alt="Download" />
+                                
+                                <h2 className='text-xl text-amber-50'>Tailwind CSS</h2>
+                        </div>
+                        <div className='flex items-center space-x-4 rounded-lg  hover:bg-gray-900 transition-colors duration-300'>
+                                
+                                        <img className='w-[50px] h-[50px] rounded-full' src="/flutter.png" alt="Download" />
+                                
+                                <h2 className='text-xl text-amber-50'>Dart</h2>
+                        </div>
+                        <div className='flex items-center space-x-4 rounded-lg  hover:bg-gray-900 transition-colors duration-300'>
+                                
+                                        <img className='w-[50px] h-[50px] rounded-full' src="/download.png" alt="Download" />
+                                
+                                <h2 className='text-xl text-amber-50'>Github</h2>
+                        </div>
         </div>
     </div>
 )
